942b7f3e95d77d58364111b38a5145cb8b0880e3
PawelMolek <pawel.molek@gmail.com>
2023-02-28 18:43:48 +0100

    [#REST Api Scala 4] First endpoint declared.

--- app/controllers/LibraryController.scala
+++ app/controllers/LibraryController.scala
@@ -1,14 +1,17 @@
 package controllers
 
-import play.api.mvc.{BaseController, ControllerComponents}
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 import javax.inject.Inject
 import javax.inject.Singleton
 import scala.collection.mutable
 import models.Book
+import play.api.libs.json._
+
 @Singleton
 class LibraryController @Inject()(val controllerComponents: ControllerComponents)
   extends BaseController {
 
+  implicit val bookListJson = Json.format[Book]
 
   val bookList = new mutable.ListBuffer[Book]()
   bookList += Book(1, "Neil deGrasse Tyson", "Astrophysics for People in a Hurry", true)
@@ -16,4 +19,11 @@ class LibraryController @Inject()(val controllerComponents: ControllerComponents
   bookList += Book(3, "Carl Sagan", "Cosmos", false)
   bookList += Book(4, "Andrzej Dragan", "Kwantechizm", false)
 
+  def getAll(): Action[AnyContent] = Action {
+    if (bookList.isEmpty) {
+      NoContent
+    } else {
+      Ok(Json.toJson(bookList))
+    }
+  }
 }
